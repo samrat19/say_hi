d0c1a912cacf988fd095281013136575c18f49ae
ATRI DAS <babinsamrat123@gmail.com>
2022-06-01 13:38:49 +0530

    makes some changes

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:say_hi/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   final String name;
@@ -41,6 +42,42 @@ class ChatScreen extends StatelessWidget {
   Widget chatBody(BuildContext context) {
     return Container(
       color: Colors.green[200],
+      child: ListView.separated(
+        separatorBuilder: (_,index)=>const SizedBox(height: 1,),
+        itemBuilder: (_,index)=>Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              if(messages[index].isByMe)
+                const Spacer(),
+              Flexible(
+                flex: 4,
+                child: Container(
+                  color: Colors.amber[100],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: messages[index].isByMe?CrossAxisAlignment.end:CrossAxisAlignment.start,
+                      children: [
+                        Text(messages[index].name,style: const TextStyle(
+                          fontSize: 20,
+                        ),),
+                        Text(messages[index].text,style: const TextStyle(
+                          fontSize: 17,
+                        ),),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              if(!messages[index].isByMe)
+                const Spacer(),
+            ],
+          ),
+        ),
+        itemCount: messages.length,
+      ),
     );
   }
 
---
+++ lib/message_model.dart
@@ -0,0 +1,11 @@
+class MessageModel {
+  final String text;
+  final String name;
+  final bool isByMe;
+
+  MessageModel({
+    required this.text,
+    required this.name,
+    required this.isByMe,
+  });
+}
