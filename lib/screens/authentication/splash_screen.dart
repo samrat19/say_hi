466308a96a2b6fae337d6dd35a6050262c7cffba
ATRI DAS <babinsamrat123@gmail.com>
2022-06-02 18:57:39 +0530

    makes some changes

--- lib/screens/authentication/splash_screen.dart
+++ lib/screens/authentication/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:say_hi/screens/authentication/welcome_screen.dart';
@@ -6,15 +9,14 @@ import 'package:say_hi/screens/chat/home_screen.dart';
 
 import '../../logic/services/auth_service.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class CheckStatus extends StatefulWidget {
+  const CheckStatus({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<CheckStatus> createState() => _CheckStatusState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-
+class _CheckStatusState extends State<CheckStatus> {
   AuthService get authService => GetIt.I<AuthService>();
 
   @override
@@ -24,8 +26,8 @@ class _SplashScreenState extends State<SplashScreen> {
     ///todo if login found route to home screen
     return StreamBuilder<User?>(
       stream: authService.userAuthStream,
-      builder: (BuildContext context, AsyncSnapshot<User?> snapshot){
-        if(snapshot.hasError){
+      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.hasError) {
           return Scaffold(
             backgroundColor: Colors.blueGrey[700],
             body: const Center(
@@ -38,24 +40,11 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
           );
-        }else if(snapshot.data == null){
+        } else if (snapshot.data == null) {
           return const WelcomeScreen();
-        }else if(snapshot.data != null){
+        } else if (snapshot.data != null) {
           return const HomeScreen();
-        }else if(snapshot.connectionState == ConnectionState.waiting){
-          return Scaffold(
-            backgroundColor: Colors.blueGrey[700],
-            body: const Center(
-              child: Text(
-                'Say Hi',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 120,
-                ),
-              ),
-            ),
-          );
-        }else{
+        } else {
           return Scaffold(
             backgroundColor: Colors.blueGrey[700],
             body: const Center(
@@ -71,21 +60,41 @@ class _SplashScreenState extends State<SplashScreen> {
         }
       },
     );
-    /*return authStream.map(
-      data: (_) => _.value == null ? const WelcomeScreen() : const HomeScreen(),
-      error: (_) => const LoginScreen(),
-      loading: (_) => Scaffold(
-        backgroundColor: Colors.blueGrey[700],
-        body: const Center(
-          child: Text(
-            'Say Hi',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 120,
-            ),
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  initState() {
+    Timer(
+      const Duration(seconds: 2),
+      () => Navigator.of(context).pushReplacement(
+        CupertinoPageRoute(builder: (_) => const CheckStatus()),
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[700],
+      body: const Center(
+        child: Text(
+          'Say Hi',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 120,
           ),
         ),
       ),
-    );*/
+    );
   }
 }
--- lib/screens/chat/home_screen.dart
+++ lib/screens/chat/home_screen.dart
@@ -1,27 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:say_hi/logic/services/auth_service.dart';
+import 'package:say_hi/screens/authentication/splash_screen.dart';
 
 import 'my_chats.dart';
 
 class HomeScreen extends StatefulWidget {
-
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
-
-
+class _HomeScreenState extends State<HomeScreen> {
   AuthService get authService => GetIt.I<AuthService>();
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()async{
+      onTap: () async {
         await authService.signOut();
+        Navigator.popUntil(context, (route) => route.isFirst);
+        Navigator.of(context).pushReplacement(
+          CupertinoPageRoute(
+            builder: (_) => const CheckStatus(),
+          ),
+        );
       },
       child: Scaffold(
         appBar: AppBar(
